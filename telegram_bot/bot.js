4a3f2128a9415d991926cdcf974de303c60165ba
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-09-14 21:27:19 +0300

    bot logic

--- telegram_bot/bot.js
+++ telegram_bot/bot.js
@@ -1,1 +1,5 @@
-console.log(1234)
+console.log('start bot')
+console.log('/start bot')
+console.log('/info bot')
+console.log('/end bot')
+console.log('stop bot')
--- telegram_bot/data-base.js
+++ telegram_bot/data-base.js
@@ -3,3 +3,13 @@ console.log('initialize database')
 console.log('update database')
 console.log('update database 2')
 console.log('update database 3')
+
+const database = 23
+
+if (database === 23) {
+    console.log('database is 23')
+} else if (database === 12) {
+    console.log('database is 23, but it is 12')
+} else {
+    console.log('database is not 23, and it is not 12, so, nothing')
+}
