9276e1651bb0fbef7e4db56697e7a62580f41d4f
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-09-26 18:37:04 +0300

    add comments

--- main/script.js
+++ main/script.js
@@ -2,6 +2,7 @@ console.log('random log in terminal for main page')
 
 console.log('NEVER CONNECT AUTH REG HERE')
 
+// Old master code
 console.log('56')
 console.log('78')
 console.log('91')
@@ -9,8 +10,10 @@ console.log('23')
 console.log('23')
 console.log('64')
 
+// New master code
 console.log('222222222222')
 
+// auth_reg_app code
 console.log('1')
 console.log('2')
 console.log('3')
