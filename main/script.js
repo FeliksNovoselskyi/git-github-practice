4103d782f306c5a2f9d3908dce51cbb6af2c1ed2
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-09-26 18:22:17 +0300

    main app change in master

--- main/script.js
+++ main/script.js
@@ -1,3 +1,10 @@
 console.log('random log in terminal for main page')
 
 console.log('NEVER CONNECT AUTH REG HERE')
+
+console.log('56')
+console.log('78')
+console.log('91')
+console.log('23')
+console.log('23')
+console.log('64')
